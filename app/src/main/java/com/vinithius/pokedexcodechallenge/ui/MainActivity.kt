40d1575e85e5b5a23702b50b46f996413b2b9bec
Vinithius <marcos.vinithius@gmail.com>
2022-06-16 17:21:49 +0100

    First commit

---
+++ app/src/main/java/com/vinithius/pokedexcodechallenge/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vinithius.pokedexcodechallenge.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vinithius.pokedexcodechallenge.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
