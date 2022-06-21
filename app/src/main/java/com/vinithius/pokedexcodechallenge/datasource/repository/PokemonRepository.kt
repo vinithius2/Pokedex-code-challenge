b1562230d2fb879b6911a67154dee5624fa5c639
Vinithius <marcos.vinithius@gmail.com>
2022-06-22 00:02:50 +0100

    Add functions in repository and viewmodel

--- app/src/main/java/com/vinithius/pokedexcodechallenge/datasource/repository/PokemonRepository.kt
+++ app/src/main/java/com/vinithius/pokedexcodechallenge/datasource/repository/PokemonRepository.kt
@@ -1,10 +1,12 @@
 package com.vinithius.pokedexcodechallenge.datasource.repository
 
 import android.content.Context
+import android.net.Uri
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.liveData
 import com.vinithius.pokedexcodechallenge.BuildConfig
+import com.vinithius.pokedexcodechallenge.datasource.response.Damage
 import com.vinithius.pokedexcodechallenge.datasource.response.Pokemon
 import com.vinithius.pokedexcodechallenge.extension.getIsFavorite
 import com.vinithius.pokedexcodechallenge.pagination.PokemonPagingSource
@@ -18,6 +20,32 @@ class PokemonRepository(private val remoteDataSource: PokemonRemoteDataSource) {
                 PokemonPagingSource(remoteDataSource)
             }
         ).liveData
+
+    suspend fun getPokemonDetail(id: Int): Pokemon {
+        val pokemon = remoteDataSource.getPokemonDetail(id)
+        val encountersRequest = remoteDataSource.getPokemonEncounters(id)
+        val characteristicRequest = remoteDataSource.getPokemonCharacteristic(id)
+        val specieRequest = remoteDataSource.getPokemonSpecies(id)
+        Uri.parse(specieRequest.evolution_chain.url).pathSegments.getOrNull(3)?.let {
+            remoteDataSource.getPokemonEvolution(it.toInt()).let { evolution_response ->
+                pokemon.apply { evolution = evolution_response }
+            }
+        }
+        val damageList: MutableList<Damage> = mutableListOf()
+        pokemon.types?.forEach { type_list ->
+            remoteDataSource.getPokemonDamageRelations(type_list.type.name).let {
+                it.type = type_list.type
+                damageList.add(it)
+            }
+        }
+        pokemon.apply {
+            encounters = encountersRequest
+            characteristic = characteristicRequest
+            specie = specieRequest
+            damage = damageList.toList()
+        }
+        return pokemon
+    }
 
     suspend fun setFavorite(pokemon: Pokemon, context: Context?) {
         context?.let {
--- app/src/main/java/com/vinithius/pokedexcodechallenge/ui/PokemonViewModel.kt
+++ app/src/main/java/com/vinithius/pokedexcodechallenge/ui/PokemonViewModel.kt
@@ -3,6 +3,7 @@ package com.vinithius.pokedexcodechallenge.ui
 import android.content.Context
 import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
@@ -17,6 +18,20 @@ class PokemonViewModel(private val repository: PokemonRepository) : ViewModel() 
 
     var currentResult: LiveData<PagingData<Pokemon>>? = null
 
+    private val _pokemonDetailLoading = MutableLiveData<Boolean>()
+    val pokemonDetailLoading: LiveData<Boolean>
+        get() = _pokemonDetailLoading
+
+    private val _pokemonDetailError = MutableLiveData<Boolean>()
+    val pokemonDetailError: LiveData<Boolean>
+        get() = _pokemonDetailError
+
+    private var _idPokemon: Int = 0
+
+    fun setIdPokemon(value: Int) {
+        _idPokemon = value
+    }
+
     /**
      * Get pokemons list using Paging3.
      */
@@ -29,13 +44,33 @@ class PokemonViewModel(private val repository: PokemonRepository) : ViewModel() 
         return currentResult
     }
 
+    /**
+     * Set favorite pokemon to webhook.
+     */
     fun setFavorite(pokemon: Pokemon, context: Context?) {
-        try {
-            CoroutineScope(Dispatchers.IO).launch {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
                 repository.setFavorite(pokemon, context)
+            } catch (e: Exception) {
+                Log.e("setFavorite", e.toString())
             }
-        } catch (e: Exception) {
-            Log.e("setFavorite", e.toString())
+        }
+    }
+
+    /**
+     * Get all details pokemon.
+     */
+    fun getPokemon() {
+        CoroutineScope(Dispatchers.IO).launch {
+            _pokemonDetailError.postValue(false)
+            _pokemonDetailLoading.postValue(true)
+            try {
+                repository.getPokemonDetail(_idPokemon)
+            } catch (e: Exception) {
+                _pokemonDetailError.postValue(true)
+                Log.e("getPokemon", e.toString())
+            }
+            _pokemonDetailLoading.postValue(false)
         }
     }
 
